2ecefd93f12d518e09575b23ddd4c9b6f69465ac
josecantero <josedejesuscantero@gmail.com>
2024-10-09 21:20:57 -0300

    se creó la pantalla principal de la aplicación se agregaron las secciones y se inicia el código de persistencia de datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //DATA STORE
+    implementation(libs.androidx.datastore.preferences)
 }
--- app/src/main/java/com/cabudev/settingsapp/MainActivity.kt
+++ app/src/main/java/com/cabudev/settingsapp/MainActivity.kt
@@ -1,23 +1,57 @@
 package com.cabudev.settingsapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.cabudev.settingsapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+val Context.datastore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val VOLUME_LVL = "volume_lvl"
+    }
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+        initUI()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun initUI() {
+        binding.rsVolume.addOnChangeListener { _, value, _ ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveVolume(value.toInt())
+            }
+        }
+    }
+
+    private suspend fun saveVolume(volume:Int){
+        datastore.edit { preference ->
+            preference[intPreferencesKey(VOLUME_LVL)] = volume
         }
     }
 }
